6b704643e50e216e4bf6a6313459796236aaba57
Houda-Ahbella <Houdasafi555@gmail.com>
2022-03-15 13:50:25 +0000

    Ajoutez des fichiers projet.

---
+++ Web1/Models/Livre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web1.Models
+{
+    public class Livre
+    {
+        public int LivreID { get; set; }
+        public string nom { get; set; }
+        public string image { get; set; }
+        public Livre()
+        {
+
+        }
+        public Livre(string n, int id, string img)
+        {
+            LivreID = id;
+            nom = n;
+            image = img;
+
+        }
+    }
+}
